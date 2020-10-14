9435b27859ef778bd33425872dc0bd5b1bbd9740
bmanc <b.mancuso37@gmail.com>
2020-10-14 22:02:46 +0200

    DB interface - GameData - User : update

--- java_src/entity/GameData.java
+++ java_src/entity/GameData.java
@@ -1,5 +1,86 @@
 package entity;
 
-public class GameData {
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class GameData implements Serializable{
+    private int id;
+    private String name;
+    private List<Session> sessions;
+    private int numPlayers;
+    private Map<String, Integer> playerPoints;
     
+    private static final long serialVersionUID = 1L;
+    private final static int MAX_PLAYERS = 6;
+    private final static int MIN_PLAYERS = 2;
+    
+    public GameData(){
+        sessions = new LinkedList<>();
+        playerPoints = new HashMap<>();
+    }
+    
+    public GameData(int id) {
+        this();
+        this.id = id;
+    }
+    
+    public GameData(String name, int numPlayers) {
+        this();
+        if(numPlayers < MIN_PLAYERS || numPlayers > MAX_PLAYERS){
+            throw new IllegalArgumentException("numPlayers must be in range 2 - 6. Passed value: " + numPlayers);
+        }
+        this.name = name;
+        this.numPlayers = numPlayers;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumPlayers() {
+        return numPlayers;
+    }
+
+    public void setNumPlayers(int numPlayers) {
+        this.numPlayers = numPlayers;
+    }
+    
+    public void addPlayer(String nickname) {
+        playerPoints.put(nickname, null);
+    }
+    
+    public void removePlayer(String nickname) {
+        playerPoints.remove(nickname);
+    }
+    
+    public void setPoints(String nickname, int points) {
+        playerPoints.put(nickname, points);
+    }
+    
+    public int getPoints(String nickname) {
+        return playerPoints.get(nickname);
+    }
+    
+    public List<String> getPlayersList() {
+        return new LinkedList<>(playerPoints.keySet());
+    }
+    
+    public void addSession(Session s){
+        sessions.add(s);
+    }
 }
