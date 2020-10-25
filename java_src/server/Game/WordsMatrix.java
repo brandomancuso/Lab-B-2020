40e6b2694e77f590d52e3c47c492db612966ec3b
acer <acer@LAPTOP-JV1U0EMD.homenet.telecomitalia.it>
2020-10-25 12:30:49 +0100

    Aggiunta dell'alogritmo di derivazione dalla griglia

--- java_src/server/Game/WordsMatrix.java
+++ java_src/server/Game/WordsMatrix.java
@@ -1,9 +1,18 @@
-package server.Game;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Stack;
 
+/* for testing
+ * 
+private String[][] matrix;
+public WordsMatrix (String[][] grid)
+	{
+  	matrix=grid;
+	}
+*/
 public class WordsMatrix {
         //Letter Schema
-        private String[][] letter=
+        private String[][] diceFaces=
         { 
             {"B","A","O","O","Q","M"},
             {"U","T","E","S","L","P"},
@@ -21,19 +30,20 @@ public class WordsMatrix {
             {"N","O","L","G","U","E"},
             {"D","C","M","P","A","E"},
             {"E","R","I","N","S","H"}
-        };  
+        }; 
 	private static int DIM = 4;
 	private Dice[] dices;
 	private Dice[][] matrix;
+
 	
 	public WordsMatrix(){
 		dices = new Dice[DIM*DIM];
 		matrix = new Dice[DIM][DIM];
 		for(int i = 0; i<DIM*DIM; i++) {
-			dices[i] = new Dice(letter[i]);
+			dices[i] = new Dice(diceFaces[i]);
 		}
 	}
-	
+
 	public void shake() {
 		Random gen = new Random();
 		int upperBound = DIM*DIM -1;
@@ -59,16 +69,179 @@ public class WordsMatrix {
 		}
             return gridTmp;
 	}
-        
-        public boolean isAllowed (String wordFound)
-        {
-            //check if the word could be derived from the matrix
-            return true;
-        }
-
+	
 	private void swap(int select, int bound) {
 		Dice tmp = dices[bound];
 		dices[bound] = dices[select];
 		dices[select] = tmp;
 	}
+	
+    public boolean isAllowed (String wordFound)
+     {
+    	Stack<Coordinate> cellToVisit=new Stack<>();
+    	HashSet<Coordinate> cellVisited=new HashSet<>();
+    	char[] wordFoundArray=wordFound.toCharArray();
+   	 	Coordinate currentCoordinate;
+   	 	Boolean flag=false;
+    	
+    	//check the start cells in the all matrix
+    	for(int i=0;i<DIM;i++)
+    		for(int j=0;j<DIM;j++)
+    		{
+    			if(matrix[i][j].equals(String.valueOf(wordFoundArray[0])))
+    				cellToVisit.add(new Coordinate(i, j,0,DIM));
+    		}
+    	
+    	//search for a walkable paths		
+         do
+    		{ 	
+        	 Stack<Coordinate> cellToVisitClone;
+        	 for(int i=cellToVisit.peek().getIndexLetter()+1 ; i< wordFound.length() ; i++)
+        	 	{
+	    			currentCoordinate=cellToVisit.pop();
+	    			cellVisited.add(currentCoordinate);
+	    			cellToVisitClone=(Stack<Coordinate>)cellToVisit.clone();
+	    			cellToVisit=findLetter(currentCoordinate,i,String.valueOf(wordFoundArray[i]),cellToVisit,cellVisited);
+	    			if(cellToVisit.equals(cellToVisitClone))
+	    			{
+	    				flag=false;
+	    				break;//to avoid doing more useless research
+	    			}
+	    			else
+	    				flag=true;
+        	 	}
+    		}while(!cellToVisit.isEmpty() && flag != true);
+         
+         return flag;//if the stack is empty and any walkable paths were found, it means that the word composed is wrong
+     }
+    
+    private Stack<Coordinate> findLetter(Coordinate currentCoordinate,int indexLetter,String letter,Stack<Coordinate> cellToVisit,HashSet<Coordinate> cellVisited) 
+    {
+    	int x=currentCoordinate.getX(), y=currentCoordinate.getY();
+		Coordinate coordinateTmp;
+		
+    	switch(currentCoordinate.getPosition())
+    	{
+    		case UPPER_LEFT:
+    			for(int i=0;i<2;i++)
+    	    		for(int j=1;j<2;j++)
+    	    		{
+    	    			if (matrix[x+i][y+j].equals(letter))
+    	    			{
+    	    				coordinateTmp=new Coordinate(x+i, y+j,indexLetter ,DIM);
+    	    				if (!cellVisited.contains(coordinateTmp))
+    	    					cellToVisit.add(coordinateTmp);
+    	    			}
+    	    		}
+    	    	break;
+
+    		case UPPER:
+    			for(int i=0;i<2;i++)
+    	    		for(int j=-1;j<2;j++)
+    	    		{
+    	    			if (matrix[x+i][y+j].equals(letter) && (x!=x+i || y!=y+j))
+    	    			{
+    	    				coordinateTmp=new Coordinate(x+i, y+j,indexLetter ,DIM);
+    	    				if (!cellVisited.contains(coordinateTmp))
+    	    					cellToVisit.add(coordinateTmp);
+    	    			}
+    	    		}
+    	    	break;
+    	    
+    		case UPPER_RIGHT:
+    			for(int i=0;i<2;i++)
+    	    		for(int j=-1;j<1;j++)
+    	    		{
+    	    			if (matrix[x+i][y+j].equals(letter) && (x!=x+i || y!=y+j))
+    	    			{
+    	    				coordinateTmp=new Coordinate(x+i, y+j,indexLetter ,DIM);
+    	    				if (!cellVisited.contains(coordinateTmp))
+    	    					cellToVisit.add(coordinateTmp);
+    	    			}
+    	    		}
+    	    	break;
+    	    
+    		case CENTER_LEFT:
+    			for(int i=-1;i<2;i++)
+    	    		for(int j=0;j<2;j++)
+    	    		{
+    	    			if (matrix[x+i][y+j].equals(letter) && (x!=x+i || y!=y+j))
+    	    			{
+    	    				coordinateTmp=new Coordinate(x+i, y+j,indexLetter ,DIM);
+    	    				if (!cellVisited.contains(coordinateTmp))
+    	    					cellToVisit.add(coordinateTmp);
+    	    			}
+    	    		}
+    	    	break;
+    	    	
+    		case CENTER:
+    			for(int i=-1;i<2;i++)
+    	    		for(int j=-1;j<2;j++)
+    	    		{
+    	    			if (matrix[x+i][y+j].equals(letter) && (x!=x+i || y!=y+j))
+    	    			{
+    	    				coordinateTmp=new Coordinate(x+i, y+j,indexLetter ,DIM);
+    	    				if (!cellVisited.contains(coordinateTmp))
+    	    					cellToVisit.add(coordinateTmp);
+    	    			}
+    	    		}
+    	    	break;
+    	    
+    		case CENTER_RIGHT:
+    			for(int i=-1;i<2;i++)
+    	    		for(int j=-1;j<1;j++)
+    	    		{
+    	    			if (matrix[x+i][y+j].equals(letter) && (x!=x+i || y!=y+j))
+    	    			{
+    	    				coordinateTmp=new Coordinate(x+i, y+j,indexLetter ,DIM);
+    	    				if (!cellVisited.contains(coordinateTmp))
+    	    					cellToVisit.add(coordinateTmp);
+    	    			}
+    	    		}
+    	    	break;
+    	    	
+    		case LOWER_LEFT:
+    			for(int i=-1;i<1;i++)
+    	    		for(int j=0;j<2;j++)
+    	    		{
+    	    			if (matrix[x+i][y+j].equals(letter))
+    	    			{
+    	    				coordinateTmp=new Coordinate(x+i, y+j,indexLetter ,DIM);
+    	    				if (!cellVisited.contains(coordinateTmp))
+    	    					cellToVisit.add(coordinateTmp);
+    	    			}
+    	    		}
+    	    	break;
+    	    
+    		case LOWER:
+    			for(int i=-1;i<1;i++)
+    	    		for(int j=-1;j<2;j++)
+    	    		{
+    	    			if (matrix[x+i][y+j].equals(letter) && (x!=x+i || y!=y+j))
+    	    			{
+    	    				coordinateTmp=new Coordinate(x+i, y+j,indexLetter ,DIM);
+    	    				if (!cellVisited.contains(coordinateTmp))
+    	    					cellToVisit.add(coordinateTmp);
+    	    			}
+    	    		}
+    	    	break;
+    	    	
+    		case LOWER_RIGHT:
+    			for(int i=-1;i<1;i++)
+    	    		for(int j=-1;j<1;j++)
+    	    		{
+    	    			if (matrix[x+i][y+j].equals(letter))
+    	    			{
+    	    				coordinateTmp=new Coordinate(x+i, y+j,indexLetter ,DIM);
+    	    				if (!cellVisited.contains(coordinateTmp))
+    	    					cellToVisit.add(coordinateTmp);
+    	    			}
+    	    		}
+    	    	break;
+    	}
+    	
+    	return cellToVisit;
+    }
 }
+    	
+
