01fdc317b2b5af3054cf50ecb811918066eb7b94
Christian Squadrito <40888532+ChristianSquadrito@users.noreply.github.com>
2020-11-01 16:51:04 +0100

    Update ClientGameStub.java

--- java_src/client/ClientGameStub.java
+++ java_src/client/ClientGameStub.java
@@ -5,7 +5,8 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface ClientGameStub extends Remote{
-    List<String> sendWords() throws RemoteException;
+    List<String> getWords() throws RemoteException;
+    void sendWords(List<WordData> wordCheckedFound) throws RemoteException;
     void changeStateGame(StateGame stateGame) throws RemoteException
     void update(int timerValue) throws RemoteException;
     void update(String[] grid) throws RemoteException;
