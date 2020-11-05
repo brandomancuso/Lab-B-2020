854dd4f5d030b966aecf1f396e792919f439fe61
bmanc <b.mancuso37@gmail.com>
2020-11-05 20:21:31 +0100

    AND CONFIGURATION NETBEANS BOIA

---
+++ java_src/database/DatabaseConfig.java
@@ -0,0 +1,26 @@
+package database;
+
+class DatabaseConfig {
+    String host;
+    String user;
+    String pswd;
+    
+    public DatabaseConfig(){
+        
+    }
+
+    public DatabaseConfig setHost(String host) {
+        this.host = host;
+        return this;
+    }
+
+    public DatabaseConfig setUser(String user) {
+        this.user = user;
+        return this;
+    }
+
+    public DatabaseConfig setPswd(String pswd) {
+        this.pswd = pswd;
+        return this;
+    } 
+}
