6147061966189b04eb409ddb2bb8ad48e62aae5f
Edoardo <Edoardo@LAPTOP-B8G70VNK>
2020-11-07 18:32:17 +0100

    definition of client interfaces

--- java_src/client/ClientGameStub.java
+++ java_src/client/ClientGameStub.java
@@ -8,9 +8,12 @@ import java.util.List;
 import java.util.Map;
 
 public interface ClientGameStub extends Remote{
-    List<String> getWords() throws RemoteException;
-    void updateWords(Map<String, List<WordData>> wordCheckedFound) throws RemoteException; 
-    //void changeStateGame(StateGame stateGame) throws RemoteException;
-    void update(int timerValue) throws RemoteException;
-    void update(String[] grid) throws RemoteException;
+    static final int SESSION = 0;//transito in schrmata di gioco
+    static final int RESULT = 1;//transito in schermata risultati sessione
+    
+    List<String> getWords() throws RemoteException;//tutte le parole trovate dall'utente --> lista 
+    void updateSessionResults(Map<String, List<WordData>> wordCheckedFound) throws RemoteException;//aggiornamento finestra fine sessione --> logica x visualizzare risultati della sessione
+    void changeGameState(int gameState) throws RemoteException;
+    void update(int timerValue) throws RemoteException;//valore sec x sec dei timer (tutti)
+    void update(String[] grid) throws RemoteException;//mi passi le lettere da mettere nela griglia grafica
 }
