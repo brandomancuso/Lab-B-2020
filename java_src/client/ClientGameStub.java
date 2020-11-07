8486b3d35544ed1ef88bccf6a1819844d8351f31
Edoardo <Edoardo@LAPTOP-B8G70VNK>
2020-11-07 18:06:55 +0100

    GUI initial design - part7

--- java_src/client/ClientGameStub.java
+++ java_src/client/ClientGameStub.java
@@ -5,10 +5,11 @@ import entity.WordData;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
+import java.util.Map;
 
 public interface ClientGameStub extends Remote{
     List<String> getWords() throws RemoteException;
-    void sendWords(List<WordData> wordCheckedFound) throws RemoteException;
+    void updateWords(Map<String, List<WordData>> wordCheckedFound) throws RemoteException; 
     //void changeStateGame(StateGame stateGame) throws RemoteException;
     void update(int timerValue) throws RemoteException;
     void update(String[] grid) throws RemoteException;
