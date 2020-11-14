e69dfbd1c4d0c8f9891b094e0701c98bc8f90b1c
Edoardo <Edoardo@LAPTOP-B8G70VNK>
2020-11-14 16:10:43 +0100

    updates

--- java_src/client/ClientGameImpl.java
+++ java_src/client/ClientGameImpl.java
@@ -33,13 +33,18 @@ public class ClientGameImpl extends UnicastRemoteObject implements ClientGameStu
     }
 
     @Override
-    public void update(int timerValue) throws RemoteException {
-
+    public void updateTimer(int timerValue) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public void update(String[] grid) throws RemoteException {
+    public void updateGrid(String[] grid) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
+    @Override
+    public void updateLobby(List<String> nickName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
